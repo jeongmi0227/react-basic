3f8d3c9bf06689cea24dc888eef6ae8f56d74c30
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-05-26 16:37:01 -0400

    Add lifecycle metod:componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// What is component?
+// A component is a self-contained piece of code that returns some visual UI representation of HTML,CSS,JS
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// rendering concept
+// Developer really understands when React is rendering and re-rendering the website.
+class App extends Component{
+  // constructor will be invovled first
+  constructor() {
+    super();
+
+    // local state
+    this.state = {
+      monsters: [],
+    };
+  }
+  // will run whenever the component mounts
+  // Mounting is the first time a component gets placed onto the top
+  // First time React renders a component onto the page that is mounting.
+  // It only happens once througout the components life.
+  // The only time when a coponent might re mount is if it's been unmounted it, 
+  // meaning it's been completely removed form the DOM.
+  componentDidMount() { 
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      }, () => { 
+        console.log(this.state);
+      }));
+  }
+
+  // The way that react to text it that there's a change is that uses 
+  // JavaScript underlying reference by memeory for an object.
+  // In memeory, whenever we instantiate a variable
+  // state is diffent is if there is a comopletely different object in memeory.
+  
+  render() {
+    return (
+      <div className="App">
+        {
+          // callback function 
+          this.state.monsters.map((monster) => { 
+            return <div key={monster.id}><h1>{monster.name}</h1></div>;
+            // why do we need key? 
+            // optimizing purpose when it comes to re rendering or updating components
+            // using this key is able to be much more performant and efficient when it
+            // comes to differentiating these components from each other and then figuring out 
+            // which ones to render appropriately when their value update.
+          })}
+          
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// React is the engine that runs how these applications get built.
+// React Dom is specifying that engine should be directed towards web related applications.
+// These two libraries combined is what actually allows us to build out all of our web applciation in React.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,12 +9,29 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // StrictMode means that whatever inside of this strict mode, 
+  // test two tags here, you're going to essentially add these
+  // additional checks and warnings that React says in case you're using or deprecated code.
+  // <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  </>
+    
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// Webpack is essentially modular being a chunk
+// make suer that optimize it and break it into the self-contained protions of it so that
+// when the user is trying to access one part of it, it only gets the relevant Javascript for that part.
+
+
+// logos are more so relat4ed to manifest.json file
+// The manifest.json file is a way for us to actually be progressive, web app compliant and all
+// The progressive web app is that some people want to be able to down load web application onto the desktop
+// so that they can access that offline use applicaion not through browser
