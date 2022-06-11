144c6fd3fad6b9d6ac1c208831c58f103bf1a449
Jeong Mi Park <parkjeongmi0227@gmail.com>
2022-06-10 23:36:40 -0400

    convert to Functional component

--- src/App.js
+++ src/App.js
@@ -1,84 +1,111 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState } from 'react';
+// useState essentially gives us a ability to encapsulate local state in a functional component.
 import logo from './logo.svg';
 import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 // What is component?
 // A component is a self-contained piece of code that returns some visual UI representation of HTML,CSS,JS
+const App = () => {
+  const [searchField, setSearchField] = useState(''); //[value,setValue]
+  
+  const onSearchChange=(event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  }
+
+  return (
+      <div className="App">
+      <h1 className='app-title'>Monster</h1>
+      <SearchBox onChageHanlder={onSearchChange} placeholder='search monsters'className='search-box'/>
+      </div>
+  )
+}
+
+// pure function 
+// take some arguments and return exact same thing, no matter how many times it gets called.
+// A function is considered pure when everything that dictates what it returns is completely isolated from what gets passed into it.
+// return should solely be dependent on the props being passed in.
+
+// impure function 
+// if it is to modify or rely on something outside of its scope and beyone the parameters being passed into it
+// side effect is when a function creates some kind of effect outside of its scope.
+// Use hooks to create impure functions.
 
 // rendering concept
 // Developer really understands when React is rendering and re-rendering the website.
-class App extends Component{
-  // constructor will be invovled first
-  constructor() {
-    super();
+// class App extends Component{
+//   // constructor will be invovled first
+//   constructor() {
+//     super();
 
-    // local state
-    this.state = {
-      monsters: [],
-      searchField:''
-    };
+//     // local state
+//     this.state = {
+//       monsters: [],
+//       searchField:''
+//     };
 
-  }
-  // will run whenever the component mounts
-  // Mounting is the first time a component gets placed onto the top
-  // First time React renders a component onto the page that is mounting.
-  // It only happens once througout the components life.
-  // The only time when a coponent might re mount is if it's been unmounted it, 
-  // meaning it's been completely removed form the DOM.
-  componentDidMount() { 
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then((users) => this.setState(() => {
-        return { monsters: users }
-      }, () => { 
-        // console.log(this.state);
-      }));
-  }
-  // Optimization2
-  // declare a function (once it is initialized when class component initializes, it will not re-render)
-  // anonymous functions will be re-render whenever call is being called.
-  onSearchChange=(event) => {
-    // console.log(event.target.value);
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    })
-    // this.state.monsters.filter((monster) => event.target.value == monster);
-  }
-  // The way that react to text it that there's a change is that uses 
-  // JavaScript underlying reference by memeory for an object.
-  // In memeory, whenever we instantiate a variable
-  // state is diffent is if there is a comopletely different object in memeory.
+//   }
+//   // will run whenever the component mounts
+//   // Mounting is the first time a component gets placed onto the top
+//   // First time React renders a component onto the page that is mounting.
+//   // It only happens once througout the components life.
+//   // The only time when a coponent might re mount is if it's been unmounted it, 
+//   // meaning it's been completely removed form the DOM.
+//   componentDidMount() { 
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(response => response.json())
+//       .then((users) => this.setState(() => {
+//         return { monsters: users }
+//       }, () => { 
+//         // console.log(this.state);
+//       }));
+//   }
+//   // Optimization2
+//   // declare a function (once it is initialized when class component initializes, it will not re-render)
+//   // anonymous functions will be re-render whenever call is being called.
+//   onSearchChange=(event) => {
+//     // console.log(event.target.value);
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     })
+//     // this.state.monsters.filter((monster) => event.target.value == monster);
+//   }
+//   // The way that react to text it that there's a change is that uses 
+//   // JavaScript underlying reference by memeory for an object.
+//   // In memeory, whenever we instantiate a variable
+//   // state is diffent is if there is a comopletely different object in memeory.
   
-  render() {
-    // Optimization1 readable
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    // Filtering down a list, always want a filter from the full list.
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-    return (
-      <div className="App">
-        <h1 className='app-title'>Monster</h1>
-        <SearchBox onChageHanlder={onSearchChange} placeholder='search monsters'className='search-box'/>
-        {/* {
-          // callback function 
-          filteredMonsters.map((monster) => { 
-            return <div key={monster.id}><h1>{monster.name}</h1></div>;
-            // why do we need key? 
-            // optimizing purpose when it comes to re rendering or updating components
-            // using this key is able to be much more performant and efficient when it
-            // comes to differentiating these components from each other and then figuring out 
-            // which ones to render appropriately when their value update.
-          })} */}
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
-  }
+//   render() {
+//     // Optimization1 readable
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     // Filtering down a list, always want a filter from the full list.
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Monster</h1>
+//         <SearchBox onChageHanlder={onSearchChange} placeholder='search monsters'className='search-box'/>
+//         {/* {
+//           // callback function 
+//           filteredMonsters.map((monster) => { 
+//             return <div key={monster.id}><h1>{monster.name}</h1></div>;
+//             // why do we need key? 
+//             // optimizing purpose when it comes to re rendering or updating components
+//             // using this key is able to be much more performant and efficient when it
+//             // comes to differentiating these components from each other and then figuring out 
+//             // which ones to render appropriately when their value update.
+//           })} */}
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
 
-}
+// }
 
 
 export default App;
